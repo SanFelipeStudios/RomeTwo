4180576f13f9e7a7ebd960e2ee3a03aaa22a47da
Daniel <djdusharm@outlook.com>
2023-04-25 22:26:23 -0400

    Initial Commit

---
+++ RomeTwo/RomeTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomeTwoApp.swift
+//  RomeTwo
+//
+//  Created by Daniel Dusharm on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RomeTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
