62a11b3177c58fda43804cd603ff5c3d51a5ec16
nicholasri <rillera.nicholas@gmail.com>
2023-08-08 18:40:21 -0500

    Removed Realm from project

--- RomeTwo/RomeTwoApp.swift
+++ RomeTwo/RomeTwoApp.swift
@@ -1,24 +1,11 @@
 import RealmSwift
 import SwiftUI
 
-// MARK: Atlas App Services (Optional)
-// The App Services App. Change YOUR_APP_SERVICES_APP_ID_HERE to your App Services App ID.
-// If you don't have a App Services App and don't wish to use Sync for now,
-// you can change this to:
-let app: RealmSwift.App? = RealmSwift.App(id: "rometwo-pxipu")
-
-// MARK: Main Views
-/// The main screen that determines whether to present the SyncContentView or the LocalOnlyContentView.
 @main
 struct RomeTwoApp: SwiftUI.App {
     var body: some Scene {
         WindowGroup {
-            // Using Sync?
-            if let app = app {
-                SyncContentView(app: app)
-            } else {
-                LocalOnlyContentView()
-            }
+            CurationView()
         }
     }
 }
